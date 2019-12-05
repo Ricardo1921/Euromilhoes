0ce87bcfd276a3f9df4526ba5f67d6ab0d579b83
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2019-12-05 11:29:21 +0000

    Add files via upload

---
+++ Euromilhoes Finau/Euromilhoes Finau/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Euromilhoes_Finau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int nestrela = 0, nnumeros = 0;
+
+        private void estrela(object sender, EventArgs e)
+        {
+            Label temp = (Label)sender;
+            if (temp.Text =="X")
+            {
+                temp.Text = "";
+                nestrela--;
+
+            }
+            else if (nestrela<2)
+            {
+                temp.Text = "X";
+                nestrela++;
+            }
+            teste.Text = temp.Tag.ToString();
+        }
+
+        private void numeros(object sender, EventArgs e)
+        {
+            Label temp = (Label)sender;
+            if (temp.Text == "X")
+            {
+                temp.Text = "";
+                nnumeros--;
+            }
+            else if (nnumeros < 5)
+            {
+                temp.Text = "X";
+                nnumeros++;
+            }
+            teste.Text = temp.Tag.ToString();
+        }
+    }
+}
